6dd7f746f5e64b03457943fb2f2d3e7de083e0fb
krijan.prajapati@4th-valley.com <krijan.prajapati@4th-valley.com>
2024-08-01 12:36:03 +0545

    feature(redux setup)

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,18 +4,24 @@ const config = {
     "../src/ui/component/**/*.stories.@(js|jsx|ts|tsx)",
     "../src/ui/container/**/*.stories.@(js|jsx|ts|tsx)",
   ],
+
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "storybook-addon-next-router",
+    "@chromatic-com/storybook"
   ],
+
   framework: {
-    name: "@storybook/react",
+    name: "@storybook/react-vite",
     options: {},
   },
-  docs: {
-    autodocs: "tag",
-  },
+
+  docs: {},
+
+  typescript: {
+    reactDocgen: "react-docgen-typescript"
+  }
 };
 export default config;
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import { Login } from "./page/login";
+import { Provider } from 'react-redux';
+import { initializeStore } from './store';
+
+const store = initializeStore();
 
 function App() {
   return (
     <>
+       <Provider store={store}>
+ 
       {/* <h2>Tech Bar Assignment</h2> */}
       <Login />
+      </Provider>
+
     </>
   );
 }
